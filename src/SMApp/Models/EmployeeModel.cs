f93f6dd9ff834490328ebc82146e64e6d5a4a846
Yury Yatskov <Y.Yatskov@hotmail.com>
2025-11-23 22:53:02 +0200

    Sorting, filtering, paging, grouping, search.

---
+++ src/SMApp/Models/EmployeeModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMApp.Models;
+
+public class EmployeeModel
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "The {0} value cannot exceed {1} characters or be less than {2} characters.", MinimumLength = 2)]
+    public string FirstName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(100, ErrorMessage = "The {0} value cannot exceed {1} characters or be less than {2} characters.", MinimumLength = 2)]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    public string Gender { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+    public int? ReportToEmpId { get; set; }
+    public string ImagePath { get; set; } = string.Empty;
+    public int EmployeeJobTitleId { get; set; }
+}
---
+++ src/SMApp/Program.cs
@@ -0,0 +1,35 @@
+using SMApp.Components;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSyncfusionBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+SyncfusionLicenseProvider.RegisterLicense(builder.Configuration["ApiSettings:SyncfusionLicenseKey"]!);
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
